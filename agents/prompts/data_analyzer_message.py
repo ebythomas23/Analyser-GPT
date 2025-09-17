46b50513836652049b7821ef3ca652a29324c222
eby <ebykachappillil@gmail.com>
2025-09-17 23:54:10 +1000

    updated the prompt

--- agents/prompts/data_analyzer_message.py
+++ agents/prompts/data_analyzer_message.py
@@ -11,19 +11,20 @@ Here are the steps you should follow:-
 You have a code executor agent which will be running that code and will tell you if any erorrs will be there or show the output.
 Make sure that the code has a print statement in the end if the task is completed.
 Could should be like below , in a single block and no multiple block.
+
 ```python
 your-code-here
 ```
 
 3. After writing your code , pause and wait for the code executor to run it before continuing.
 
-4. If any library is not installed in the env , please make sure to do the same by providing the bash script 
-and use pip to install (like pip install pandas numpy)and after that send the code again without chnages, install the required libraries. 
-```bash
+4. If any library is not installed in the env, please make sure to do the same by providing the bash script and use pip to install(like pip install matplotlib pandas) and after that send the code again without changes , install the required libraries.
+example
+```sh
 pip install pandas numpy matplotlib
 ```
 
-5. If you are asked to create an image , please make sure to create the image in output.png and save it in wokring directory 
+5. If you are asked to create an image , please make sure to create the image in output.png and save it in working directory 
 
 6. If the code ran sucessfully , then analyse the output and continue as needed.
 
